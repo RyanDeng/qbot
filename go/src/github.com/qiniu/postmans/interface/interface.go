33a924d17c5a51b5e4a7206844f771807e317f4b
lintianzhi <lintianzhi1992@gmail.com>
2015-07-03 20:51:47 +0800

    interface

---
+++ go/src/github.com/qiniu/postmans/interface/interface.go
@@ -0,0 +1,19 @@
+package qbot
+
+type Msg struct {
+	From string
+	Msg  string
+}
+
+type GroupMsg struct {
+	GroupId string
+	From    string
+	Msg     string
+}
+
+type Postman interface {
+	SendMsg(to string, msg string) error
+	RecvMsg() chan Msg
+	SendGroupMsg(gid string, msg string) error
+	RecvGroupMsg() chan GroupMsg
+}
